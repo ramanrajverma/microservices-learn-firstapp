d6b35387b93f1a66cc0f239c0cc92dad95b9309d
ramanrajverma <raman.verma357@gmail.com>
2025-04-01 12:56:47 +0530

    updated request dto

--- Microservice.Web/Models/RequestDto.cs
+++ Microservice.Web/Models/RequestDto.cs
@@ -6,6 +6,6 @@
         public string ApiType { get; set; } = "GET";
         public string Url { get; set; }
         public object Data { get; set; }
-        public string AccessToken { get; set; }
+        public string? AccessToken { get; set; }
     }
 }
